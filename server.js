35bf30e3d11f57ece769a16f5d4a8eb56969c55c
krav maguy <flex4lease@gmail.com>
2021-02-26 06:59:47 -0600

    first commit

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
+require("dotenv").config();
 const express = require('express')
 const app = express()
 const cors = require('cors')
-require('dotenv').config()
+const mongoose = require("mongoose");
 
 app.use(cors())
 app.use(express.static('public'))
+const connection=process.env.connection
+mongoose.connect(connection, {
+  useNewUrlParser: true,useUnifiedTopology: true
+});
+if(!connection){
+  throw new Error('Required connection missing');
+} else {
+  console.log(connection)
+}
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
